d214d9f8de3972dd28833ddeaad2850d7b0239e4
CL-rajesab <rajesab@cirruslabs.io>
2023-07-11 12:19:57 +0530

    created two dropdown and added some content

---
+++ script.js
@@ -0,0 +1,41 @@
+function loadMarkdown(file) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((markdown) => {
+      const md = window.markdownit();
+      const result = md.render(markdown);
+      const updatedResult = result.replace(
+        /<img src="(.*?)"(.*?)>/g,
+        '<img src="$1" width="70%" height="auto"$2>'
+      );
+
+      document.getElementById("content").innerHTML = updatedResult;
+    })
+    .catch((error) => {
+      console.error("Error loading Markdown file:", error);
+    });
+}
+
+function handleMenuItemClick(file) {
+  loadMarkdown(file); // Load and render the Markdown file
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdowns = document.getElementsByClassName("dropdown");
+  for (let i = 0; i < dropdowns.length; i++) {
+    const dropdown = dropdowns[i];
+    const dropbtn = dropdown.querySelector(".dropbtn");
+    const dropdownContent = dropdown.querySelector(".dropdown-content");
+
+    dropbtn.addEventListener("click", function () {
+      dropdown.classList.toggle("active");
+      if (dropdown.classList.contains("active")) {
+        dropdownContent.style.display = "block";
+      } else {
+        dropdownContent.style.display = "none";
+      }
+    });
+  }
+
+  loadMarkdown("default.md");
+});
