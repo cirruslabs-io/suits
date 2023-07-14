70418bd56a2a578ac0e11dcff005b520f4f5a736
CL-rajesab <rajesab@cirruslabs.io>
2023-07-14 13:45:09 +0530

    shifted image to center

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const css = `
   .zoomable-image {
     cursor: zoom-in;
+    display: flex;
+    justify-content: center;
   }
 
   .zoomable-image.zoomed {
@@ -41,7 +43,7 @@ function loadMarkdown(file) {
           const updatedAttributes = isMobileImage
             ? 'width="250px"'
             : 'width="65%" height="auto"';
-          return `<div class="zoomable-image" style="display: flex; justify-content: center;"><img src="${src}" ${updatedAttributes}></div>`;
+          return `<div class="zoomable-image" ><img src="${src}" ${updatedAttributes}></div>`;
         }
       );
 
